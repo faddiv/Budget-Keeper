4f6e60f10c66f1e3c9f96d9ff5d9925b22a16b7e
faddiv <faddiv@gmail.com>
2017-10-04 10:57:41 +0200

    export form validation bindings finished.

--- Wallet/Wallet.Client.Angular/src/app/export/export.component.ts
+++ Wallet/Wallet.Client.Angular/src/app/export/export.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import * as moment from "moment";
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-export',
@@ -12,38 +12,87 @@ export class ExportComponent implements OnInit {
   form = new FormGroup({
     file: new FormControl("", [
       Validators.required
-    ])
+    ]),
+    rangeType: new FormControl("1", [
+      Validators.required
+    ]),
+    yearMonth: new FormGroup({
+      year: new FormControl(null, [
+        Validators.required
+      ]),
+      month: new FormControl(null, [
+        Validators.required
+      ])
+    }),
+    fromTo: new FormGroup({
+      rangeFrom: new FormControl(null, [
+        Validators.required
+      ]),
+      rangeTo: new FormControl(null, [
+        Validators.required
+      ])
+    })
   });
 
-  rangeType: number = 1;
   rangeTypes = [{
-    value: 1,
+    value: "1",
     name: "Year/Month"
   }, {
-    value: 2,
+    value: "2",
     name: "From-To"
   }];
 
-  year: number;
-  month: number;
-  rangeFrom: string;
-  rangeTo: string;
-
   constructor() {
     var now = moment();
-    this.year = now.year();
-    this.month = now.month() + 1;
-    this.rangeFrom = now.startOf("month").format("YYYY-MM-DD");
-    this.rangeTo = now.endOf("month").format("YYYY-MM-DD");
+    this.year.setValue(now.year());
+    this.month.setValue(now.month() + 1);
+    this.rangeFrom.setValue(now.startOf("month").format("YYYY-MM-DD"));
+    this.rangeTo.setValue(now.endOf("month").format("YYYY-MM-DD"));
   }
 
   get file() {
-    return <FormControl>this.form.controls.file;
-  }
-  ngOnInit() {
+    return <FormControl>this.form.get("file");
   }
 
+  get rangeType() {
+    return <FormControl>this.form.get("rangeType");
+  }
+  
+  get yearMonth() {
+    return <FormGroup>this.form.get("yearMonth");
+  }
+  get year() {
+    return <FormControl>this.yearMonth.get("year");
+  }
+  get month() {
+    return <FormControl>this.yearMonth.get("month");
+  }
+  
+  get fromTo() {
+    return <FormGroup>this.form.get("fromTo");
+  }
+  get rangeFrom() {
+    return <FormControl>this.fromTo.get("rangeFrom");
+  }
+  get rangeTo() {
+    return <FormControl>this.fromTo.get("rangeTo");
+  }
+  
+  ngOnInit() {
+    this.rangeType.valueChanges.subscribe(data => {
+      this.setEnabled(this.yearMonth, data === "1");
+      this.setEnabled(this.fromTo, data === "2");
+    });
+  }
+
+  setEnabled(control: AbstractControl, enabled: boolean) {
+    if(enabled) {
+      control.enable();
+    } else {
+      control.disable();
+    }
+  }
   download() {
-    alert(this.rangeType);
+    alert(this.form.valid);
   }
 }
