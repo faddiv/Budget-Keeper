c83d61a228f8527d81c15fc541d9a06c0a8ba7eb
faddiv <faddiv@gmail.com>
2018-04-12 08:56:29 +0200

    ArticleCommands Tests and additional planned tests.

--- Wallet/OnlineWallet.Web.Core.Tests/Modules/GeneralDataModule/Commands/ArticleCommandsUpdateArticleStatusesTests.cs
+++ Wallet/OnlineWallet.Web.Core.Tests/Modules/GeneralDataModule/Commands/ArticleCommandsUpdateArticleStatusesTests.cs
@@ -1,6 +1,12 @@
-﻿using OnlineWallet.Web.TestHelpers;
+﻿using FluentAssertions;
+using OnlineWallet.Web.TestHelpers;
+using OnlineWallet.Web.TestHelpers.Builders;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using TestStack.Dossier.Lists;
 using Xunit;
 
 namespace OnlineWallet.Web.Modules.GeneralDataModule.Commands
@@ -23,9 +29,55 @@ namespace OnlineWallet.Web.Modules.GeneralDataModule.Commands
             _fixture.Dispose();
         }
 
-        [Fact(DisplayName = nameof(Test1))]
-        public async Task Test1()
+        [Fact(DisplayName = nameof(Inserts_all_new_article_if_null_provided))]
+        public async Task Inserts_all_new_article_if_null_provided()
         {
+            _fixture.DbContext.Transactions.AddRange(TransactionBuilder.CreateListOfSize(10).BuildList());
+            _fixture.DbContext.SaveChanges();
+
+            await _articleCommands.UpdateArticleStatuses(null, CancellationToken.None);
+
+            var articles = _fixture.DbContext.Article.ToList();
+            articles.Should().HaveCount(_fixture.DbContext.Transactions.Select(e => e.Name).Distinct().Count());
+        }
+
+        [Fact(DisplayName = nameof(Inserts_only_given_articles_if_provided))]
+        public async Task Inserts_only_given_articles_if_provided()
+        {
+            _fixture.DbContext.Transactions.AddRange(TransactionBuilder.CreateListOfSize(10)
+                .TheFirst(1).WithName("alfa")
+                .TheNext(1).WithName("beta")
+                .BuildList());
+            _fixture.DbContext.SaveChanges();
+
+            await _articleCommands.UpdateArticleStatuses(new List<string> { "alfa", "beta" }, CancellationToken.None);
+
+            var articles = _fixture.DbContext.Article.ToList();
+            articles.Should().HaveCount(2);
+        }
+
+        [Fact(DisplayName = nameof(Updates_all_article_on_null_param))]
+        public async Task Updates_all_article_on_null_param()
+        {
+            Xunit.Assert.True(false);
+        }
+
+        [Fact(DisplayName = nameof(Updates_only_given_articles_if_provided))]
+        public async Task Updates_only_given_articles_if_provided()
+        {
+            Xunit.Assert.True(false);
+        }
+
+        [Fact(DisplayName = nameof(Removes_non_existent_articles_if_null_provided))]
+        public async Task Removes_non_existent_articles_if_null_provided()
+        {
+            Xunit.Assert.True(false);
+        }
+
+        [Fact(DisplayName = nameof(Removal_affect_only_provided_article_if_article_names_provided))]
+        public async Task Removal_affect_only_provided_article_if_article_names_provided()
+        {
+            Xunit.Assert.True(false);
         }
     }
 }
