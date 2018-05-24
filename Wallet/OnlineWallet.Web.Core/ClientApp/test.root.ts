1fad76b2562d847b00d2ea11f7b642ed5aa38cfb
faddiv <faddiv@gmail.com>
2018-05-24 10:07:04 +0200

    replace jest-fetch-mock with fetch-mock and isomorphic-fetch.

--- Wallet/OnlineWallet.Web.Core/ClientApp/test.root.ts
+++ Wallet/OnlineWallet.Web.Core/ClientApp/test.root.ts
@@ -1,5 +1,5 @@
 import * as Enzyme from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
-import "jest-fetch-mock";
+import "isomorphic-fetch";
 
 Enzyme.configure({ adapter: new Adapter() });
