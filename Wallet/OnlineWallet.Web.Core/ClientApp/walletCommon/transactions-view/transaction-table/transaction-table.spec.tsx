a004ed4148ec1d8b0c4689ccf86457c82d0d830f
Viktor Faddi <faddiv@gmail.com>
2018-05-29 21:26:32 +0200

    transaction-table.tsx: add moouse selection test. #89

--- Wallet/OnlineWallet.Web.Core/ClientApp/walletCommon/transactions-view/transaction-table/transaction-table.spec.tsx
+++ Wallet/OnlineWallet.Web.Core/ClientApp/walletCommon/transactions-view/transaction-table/transaction-table.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render, mount } from "enzyme";
+import { render, mount, HTMLAttributes, ReactWrapper } from "enzyme";
 import "jest-enzyme";
 
 import { TransactionTable } from "./transaction-table";
@@ -54,6 +54,37 @@ describe("TransactionSummary", () => {
         expect(rows.at(2)).toHaveClassName("selected");
         expect(rows.at(3)).not.toHaveClassName("selected");
     });
+
+    it("should highligh rows with mouse down move, move and up", () => {
+        const items: TransactionViewModel[] = createTransactions(4);
+        const transactionSummary = [];
+
+        const element = mount(<TransactionTable2 alertActions={alertActions}
+            summaryActions={summaryActions} items={items} wallets={wallets}
+            transactionSummary={transactionSummary} rowColor={getDirectionColoring} />);
+
+        const rows = element.find("tbody tr");
+
+        // Mouse down on the second row.
+        simulateEvent(rows.at(1), "mousedown");
+        const selection = items.slice(1, 2);
+        expect(summaryActions.transactionsSelected).toHaveBeenCalledWith(selection);
+        (summaryActions.transactionsSelected as jasmine.Spy).calls.reset();
+        // reducer sets the transactionSummary
+        element.setProps({
+            transactionSummary: selection
+        });
+
+        // moving to the third row.
+        simulateEvent(rows.at(2), "mouseenter");
+        expect(summaryActions.transactionsSelected).toHaveBeenCalledWith(items.slice(1, 3));
+        (summaryActions.transactionsSelected as jasmine.Spy).calls.reset();
+
+        // Releasing the mouse we stop the selection mode.
+        simulateEvent(rows.at(2), "mouseup");
+        simulateEvent(rows.at(3), "mouseenter");
+        expect(summaryActions.transactionsSelected).not.toHaveBeenCalled();
+    });
 });
 
 function createTransactions(size: number = 4) {
@@ -63,4 +94,16 @@ function createTransactions(size: number = 4) {
         { key: 3, transactionId: 3, name: "detergent", category: "cleaning", comment: "for cleaning", createdAt: "2018-05-27", price: "100", direction: -1, walletId: 1 },
         { key: 4, transactionId: 4, name: "movie ticket", category: "fun", comment: "good movie", createdAt: "2018-05-28", price: "1000", direction: 0, walletId: 2 }
     ].slice(0, size);
+}
+
+function createMouseEvent(target: any) {
+    return {
+        isDefaultPrevented: () => false,
+        target,
+        currentTarget: target
+    };
+}
+
+function simulateEvent(target: ReactWrapper<HTMLAttributes, any>, event: string) {
+    target.simulate(event, createMouseEvent(target));
 }
