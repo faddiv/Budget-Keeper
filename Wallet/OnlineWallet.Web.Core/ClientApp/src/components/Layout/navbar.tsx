a86cf823e71534e9f5828ff04a2f2d01528d2169
faddiv <faddiv@gmail.com>
2022-01-01 17:44:50 +0100

    Collapse navbar on select.

--- Wallet/OnlineWallet.Web.Core/ClientApp/src/components/Layout/navbar.tsx
+++ Wallet/OnlineWallet.Web.Core/ClientApp/src/components/Layout/navbar.tsx
@@ -3,7 +3,7 @@ import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export function MainMenu() {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark" sticky="top" className="mb-3">
+    <Navbar expand="lg" bg="dark" variant="dark" sticky="top" className="mb-3" collapseOnSelect>
       <Container>
         <Navbar.Brand as={Link} to="/">
           Wallet
@@ -11,32 +11,32 @@ export function MainMenu() {
         <Navbar.Toggle aria-controls="main-menu-nav" />
         <Navbar.Collapse id="main-menu-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/" exact>
+            <Nav.Link as={NavLink} to="/" exact eventKey="home">
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/transactions">
+            <Nav.Link as={NavLink} to="/transactions" eventKey="transactions">
               Transactions
             </Nav.Link>
             <NavDropdown title="Statistics" id="statistics-nav">
-              <NavDropdown.Item as={NavLink} to="/statistics/yearly" activeClassName="active">
+              <NavDropdown.Item as={NavLink} to="/statistics/yearly" activeClassName="active" eventKey="yearlyStats">
                 Yearly statistics
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/statistics/category" activeClassName="active">
+              <NavDropdown.Item as={NavLink} to="/statistics/category" activeClassName="active" eventKey="categoryStats">
                 Category statistics
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/statistics/articles" activeClassName="active">
+              <NavDropdown.Item as={NavLink} to="/statistics/articles" activeClassName="active" eventKey="articlesStats">
                 Articles
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Export/Import" id="export-import-nav">
-              <NavDropdown.Item as={NavLink} to="/import" activeClassName="active">
+              <NavDropdown.Item as={NavLink} to="/import" activeClassName="active" eventKey="import">
                 Import
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/export" activeClassName="active">
+              <NavDropdown.Item as={NavLink} to="/export" activeClassName="active" eventKey="export">
                 Export
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link as={NavLink} to="/wallets">
+            <Nav.Link as={NavLink} to="/wallets" eventKey="wallets">
               Wallets
             </Nav.Link>
           </Nav>
